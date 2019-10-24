15d1303e88439ff48ab11b62d15034e01ac5515f
mayabytatech <maya.k.k@lxisoft.com>
2019-10-24 11:08:40 +0530

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/illud/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.illud.repository.search;
+
+import com.illud.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
